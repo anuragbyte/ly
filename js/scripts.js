04b01342aea22d40b2624b6e59327801c64064d4
Arnab <arnab4srk@gmail.com>
2020-04-29 07:18:37 +0530

    Initial Design Completed for Landing and Course page

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function () 
+{
+    $("#login").click(function()
+    {
+        $('#loginModal').modal('show');
+        $('.container' ).addClass('blur' );
+    });
+    $(".close").click(function()
+    {
+        $("#loginModal").modal('hide');
+        $('.cont' ).removeClass('blur' );
+    });
+    $("#signup").click(function()
+    {
+        $('#signupModal').modal('show');
+        $('.container' ).addClass('blur' );
+    });
+    $(".close").click(function()
+    {
+        $("#signupModal").modal('hide');
+        $('.cont' ).removeClass('blur' );
+    });
+});
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
